c2d6f2ed0476ff15c8946d59c7b97822a6a28761
rakhat_cyanid <zhayshilik@gmail.com>
2024-04-09 23:01:37 +0500

    second commit

---
+++ lab1/index.ts
@@ -0,0 +1,10 @@
+import { Keypair } from "@solana/web3.js";
+import "dotenv/config";
+import { getKeypairFromEnvironment } from "@solana-developers/helpers";
+
+
+// const keypair = getKeypairFromEnvironment("SECRET_KEY");
+const keypair = Keypair.generate();
+
+console.log(`The public key is: `, keypair.publicKey.toBase58());
+console.log(`The secret key is: `, keypair.secretKey);
